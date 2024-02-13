37f42313d0a9d21dbeee2f6698c3cacf9c5e3174
Anh Pham <anhvietq@buffalo.edu>
2024-02-13 14:36:07 -0500

    Updates

---
+++ precache-manifest.555f95da7830364c42e5886c16ac92e2.js
@@ -0,0 +1,126 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "36b1b555ab938330eb940e198e3df43c",
+    "url": "/anhvietq/index.html"
+  },
+  {
+    "revision": "259c63e79f7d2c2f7ead",
+    "url": "/anhvietq/static/css/2.d0171c53.chunk.css"
+  },
+  {
+    "revision": "bb91aeeffcc1eb6f1ad0",
+    "url": "/anhvietq/static/css/main.690dbb8b.chunk.css"
+  },
+  {
+    "revision": "259c63e79f7d2c2f7ead",
+    "url": "/anhvietq/static/js/2.0fa549aa.chunk.js"
+  },
+  {
+    "revision": "075ee0fe56affc8020645735366f8cbd",
+    "url": "/anhvietq/static/js/2.0fa549aa.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bb91aeeffcc1eb6f1ad0",
+    "url": "/anhvietq/static/js/main.bbd3b41e.chunk.js"
+  },
+  {
+    "revision": "8bdddf06de312de6e60f",
+    "url": "/anhvietq/static/js/runtime-main.bb1b9477.js"
+  },
+  {
+    "revision": "b9610323b475ccd3c8da0c806ec77fdd",
+    "url": "/anhvietq/static/media/Anh_Pham_Resume.b9610323.pdf"
+  },
+  {
+    "revision": "dbb97076f45b2e9b86748912b164a138",
+    "url": "/anhvietq/static/media/MNewsletter1.dbb97076.png"
+  },
+  {
+    "revision": "5bb02483b22968e6d43f3e142fe1db26",
+    "url": "/anhvietq/static/media/MNewsletter2.5bb02483.png"
+  },
+  {
+    "revision": "f4a5c19fbe8e4f464b5e422b7d86aaae",
+    "url": "/anhvietq/static/media/MNewsletter3.f4a5c19f.png"
+  },
+  {
+    "revision": "adb315d35bbf4d77bce0e4876e7417d5",
+    "url": "/anhvietq/static/media/MNewsletter4.adb315d3.png"
+  },
+  {
+    "revision": "65bfaa51cba042eac187200bab750eeb",
+    "url": "/anhvietq/static/media/MWallet1.65bfaa51.png"
+  },
+  {
+    "revision": "6f8b409ea4e8b63aaa5f18fb28fe7e33",
+    "url": "/anhvietq/static/media/MWallet2.6f8b409e.png"
+  },
+  {
+    "revision": "075bcc0d661cab47d1c6cff00e3edb2e",
+    "url": "/anhvietq/static/media/MWallet3.075bcc0d.png"
+  },
+  {
+    "revision": "f28269e6c4b7e1d9c95fc1f1c0fe2dc4",
+    "url": "/anhvietq/static/media/MWallet4.f28269e6.png"
+  },
+  {
+    "revision": "5c3aa1a6e958b8d14f5b16b537bf5cd1",
+    "url": "/anhvietq/static/media/Metaintro.5c3aa1a6.png"
+  },
+  {
+    "revision": "73a18af26219a9fa2893e87e42c5d06b",
+    "url": "/anhvietq/static/media/Pokedex.73a18af2.png"
+  },
+  {
+    "revision": "3e521e7fce1d6e48214ae736b51f76c2",
+    "url": "/anhvietq/static/media/Pokedex2.3e521e7f.png"
+  },
+  {
+    "revision": "30beeddc2cba07d7deb3ba47b9ec32af",
+    "url": "/anhvietq/static/media/Pokedex4.30beeddc.png"
+  },
+  {
+    "revision": "63a86f1fdc2229621896f77f263f1236",
+    "url": "/anhvietq/static/media/Pokedex5.63a86f1f.png"
+  },
+  {
+    "revision": "a4a33b5f728ddd7adfce60d5d05219ec",
+    "url": "/anhvietq/static/media/Profile Photo.a4a33b5f.png"
+  },
+  {
+    "revision": "3a44beb39fdec1f6187456d5d5b420d3",
+    "url": "/anhvietq/static/media/Resilience.3a44beb3.png"
+  },
+  {
+    "revision": "2e1fa4592dbbc84ffdf4a333cc0a597a",
+    "url": "/anhvietq/static/media/SpaceLab.2e1fa459.png"
+  },
+  {
+    "revision": "d0c436e716938dc73f7ae5171586c65f",
+    "url": "/anhvietq/static/media/SpaceLab.d0c436e7.png"
+  },
+  {
+    "revision": "4289b314557fb9cbb368d280e29a8b61",
+    "url": "/anhvietq/static/media/SpaceLab2.4289b314.png"
+  },
+  {
+    "revision": "21a9b391257b69592e02587a86c39d21",
+    "url": "/anhvietq/static/media/Techfleet.21a9b391.png"
+  },
+  {
+    "revision": "b7c9e1e479de3b53f1e4e30ebac2403a",
+    "url": "/anhvietq/static/media/slick.b7c9e1e4.woff"
+  },
+  {
+    "revision": "ced611daf7709cc778da928fec876475",
+    "url": "/anhvietq/static/media/slick.ced611da.eot"
+  },
+  {
+    "revision": "d41f55a78e6f49a5512878df1737e58a",
+    "url": "/anhvietq/static/media/slick.d41f55a7.ttf"
+  },
+  {
+    "revision": "f97e3bbf73254b0112091d0192f17aec",
+    "url": "/anhvietq/static/media/slick.f97e3bbf.svg"
+  }
+]);
